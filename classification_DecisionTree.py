d691f478d802843ec41a01bd2873c363e930b0ae
Jiachen <Jiachen1018@outlook.com>
2018-04-30 00:19:00 +0100

    3

--- classification_DecisionTree.py
+++ classification_DecisionTree.py
@@ -1,5 +1,8 @@
 
 # 1st class: v[1]
+import operator
+import csv
+import re
 import os
 import json
 from sklearn.externals import joblib
@@ -7,10 +10,27 @@ from sklearn.decomposition import PCA
 from sklearn import tree
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
+import numpy as np
+import graphviz
+stemmer = SnowballStemmer("english")
+
+
+def getwords(doc):
+    splitter = re.compile('\\W+[0-9]*') # split with non-words
+    #print(splitter)
+    stopworddic = set(stopwords.words('english'))
+    words=[s.lower() for s in splitter.split(doc)
+           if len(s)>2 and len(s)<20 and s not in stopworddic]
+    stems = [stemmer.stem(t) for t in words]
+    return dict([(w, stems.count(w)) for w in stems])
+
 class Classification_JCL():
     def __init__(self):
         self.vec = []
-        self.toxic = []
+        self.target = []
+        #self.toxic = []
         # self.severe_toxic = []
         # self.obscene =[]
         # self.threat = []
@@ -20,28 +40,43 @@ class Classification_JCL():
         #self.key = []
         self.result = []
         self.classifier = None
-    def read_training_set(self):
-        with open('train_full.json') as json_train:
-            train = json.load(json_train)
-            for key,value in train.items():
-                self.words.append(value[0])
-                self.toxic.append(value[1])
-                # self.severe_toxic.append(value[2])
-                # self.obscene.append(value[3])
-                # self.threat.append(value[4])
-                # self.insult.append(value[5])
-                # self.identity_hate.append(value[6])
-                #self.key.append(key)
-        json_train.close()
+    # def read_training_set(self):
+    #     with open('train_full.json') as json_train:
+    #         train = json.load(json_train)
+    #         for key,value in train.items():
+    #             self.words.append(value[0])
+    #             self.target.append(value[1])
+    #             # self.severe_toxic.append(value[2])
+    #             # self.obscene.append(value[3])
+    #             # self.threat.append(value[4])
+    #             # self.insult.append(value[5])
+    #             # self.identity_hate.append(value[6])
+    #             #self.key.append(key)
+    #     json_train.close()
+    def read_csv(self):
+        comment = {}
+        path_test = 'C:\Users\home\Documents\GitHub\DataMining-Group-Project\\train1.csv'
+        path_full = 'C:\Users\home\Documents\Data Mining\\toxic comment\\train.csv'
+        with open(path_full) as Train:
+            reader = csv.DictReader(Train)
+            #labels = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+
+            for line in reader:
+                # id = line['\xef\xbb\xbfid']
+                #id = line['id']
+                self.words.append((line['comment_text']))
+                self.target.append(float(line['identity_hate']))
+        #print self.words
+
     def feature_matrix(self):
         Vector = DictVectorizer()
         self.vec = Vector.fit_transform(self.words).toarray()
 
     def tf_idf(self):
         files = os.listdir('./')
-        saved_file_name = 'doc_matrix.pkl'
+        saved_file_name = 'doc_matrix_full.pkl'
         if saved_file_name not in files:
-            tfidf_vectorizer = TfidfVectorizer(max_df=0.9, max_features=250000,min_df=0.01, stop_words='english',
+            tfidf_vectorizer = TfidfVectorizer(max_df=0.9, max_features=250000,min_df=0.01,tokenizer=getwords, stop_words='english',
                                              use_idf=True, ngram_range=(1, 1))
             tfidf_matrix = tfidf_vectorizer.fit_transform(self.words) #fit the vectorizer to synopses
             joblib.dump(tfidf_matrix, saved_file_name)
@@ -51,16 +86,30 @@ class Classification_JCL():
 
     def Decision_Tree(self):
         clf = tree.DecisionTreeClassifier()
-        clf.fit(self.vec,self.toxic)
-        self.result=clf.predict(self.vec)
-        rate = 1-sum(abs(i) for i in (self.result-self.toxic))/len(self.toxic)
-        print rate
+        clf.fit(self.vec,self.target)
+        self.classifier = clf
+        result=clf.predict(self.vec)
+        for i in result:
+            self.result.append(float(i))
 
-    def write_result(self):
-        jsObj = json.dumps(self.result)
-        fileObject = open('result_full.json', 'w')
-        fileObject.write(jsObj)
-        fileObject.close()
+        #print (self.toxic)
+        rate = sum(abs(i) for i in  map(operator.sub, self.target, self.result))/len(self.target)
+        print 1.0-rate
+
+    def plottree(self):
+        # dot_data = tree.export_graphviz(self.classifier, out_file=None,
+        #                                 feature_names=self.vec,
+        #                                 class_names=self.target,
+        #                                 filled=True, rounded=True,
+        #                                 special_characters=True)
+        dot_data = tree.export_graphviz(self.classifier, out_file=None)
+        graph = graphviz.Source(dot_data)
+        graph.render("identity_hate")
+        # def write_result(self):
+    #     jsObj = json.dumps(self.result)
+    #     fileObject = open('result_full.json', 'w')
+    #     fileObject.write(jsObj)
+    #     fileObject.close()
 
     # def Ten_fold_Validation(self):
     #     N = len(self.key)
@@ -76,8 +125,9 @@ class Classification_JCL():
 
 if __name__ == '__main__':
     Test = Classification_JCL()
-    Test.read_training_set()
-    Test.feature_matrix()
-    #Test.tf_idf()
+    Test.read_csv()
+    #Test.feature_matrix()
+    Test.tf_idf()
     Test.Decision_Tree()
+    #Test.plottree()
     #Test.write_result()
