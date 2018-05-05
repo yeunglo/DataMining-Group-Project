e432ecdf7de2b58fe54dc2bd74307d08caa5b57d
Lu Yang <29081437@qq.com>
2018-05-05 22:50:50 +0100

    knn
    multi label

--- knn_clean.py
+++ knn_clean.py
@@ -19,6 +19,7 @@ with open('train_original.json') as Train_data:
 
 # reader1={"78915629f8418c85": [{"and": 1, "hair": 1, "shaggy": 1, "also": 1, "very": 1, "some": 1,"dark": 1,"jewish": 1,"looks": 1,"imagine": 1,"you": 1,"beard": 1,"with": 1,"him": 1,"can": 1}, 1, 0, 0, 0, 0, 0], "ae6308347d82cb41": [{"about": 1, "think": 1, "your": 1, "hgbob": 1, "edit": 1, "please": 1, "some": 1, "here": 1, "you": 1, "may": 1, "see": 1, "flags": 1, "controversial": 1, "ask": 1, "the": 1, "debate": 1, "talk": 1, "theres": 1}, 1, 0, 0, 0, 0, 0], "bdf44cf93bd918a3": [{"redirect": 1, "despot": 1, "talkjohn": 1, "palaiologos": 1}, 0, 0, 0, 0, 0, 0], "291708466f6a09e4": [{"all": 1, "jimim": 1, "lout": 1}, 1, 1, 0, 0, 0, 0]}
 
+'''
 def get_data(data):
     words = []
     labels_com = []
@@ -31,6 +32,22 @@ def get_data(data):
         label_com = int(label_com, 2)
         labels_com.append(label_com)
     print(labels_com)
+    for item in words:
+        wordslist.append(str(" ".join(i for i in item)))
+    print(wordslist[0:3])
+    return wordslist, labels_com
+'''
+
+def get_data(data):
+    words = []
+    labels_com = []
+    wordslist = []
+    for key in data:
+        words.append(data[key][0])
+        label_com = data[key][1:]
+        labels_com.append(label_com)
+    # print(labels_com)
+    labels_com = np.asarray(labels_com)
     for item in words:
         wordslist.append(str(" ".join(i for i in item)))
     print(wordslist[0:3])
@@ -48,7 +65,7 @@ def split_data(input, random_state=1, shuffle=True):
     return x_train, x_test, tag_train, tag_test
 
 def knn(n, x_train, x_test, tag_train, tag_test):
-    estimator = KNeighborsClassifier(n)
+    estimator = KNeighborsClassifier(n_neighbors=n, weights='distance')
     print('estimator setup')
     estimator.fit(x_train, tag_train)
     print('estimator done')
@@ -88,19 +105,18 @@ tfidf_matrix = feature_extraction(wordslist, 'tfidf', max_df=0.4, min_df=0)
 input = [tfidf_matrix, labels_com]
 x_train, x_test, tag_train, tag_test = split_data(input)
 
-'''
-# find the best k value
-best_k = 0
-best_accuracy = 0
-for n in range(1,51):
-    accuracy = knn(n, x_train, x_test, tag_train, tag_test)
-    if accuracy > best_accuracy:
-        best_k = n
-        best_accuracy = accuracy
-print(best_k, best_accuracy)
-'''
 
-accuracy = knn(10, x_train, x_test, tag_train, tag_test)
+# find the best k value
+# best_k = 0
+# best_accuracy = 0
+# for n in range(1,31):
+#     accuracy = knn(n, x_train, x_test, tag_train, tag_test)
+#     if accuracy > best_accuracy:
+#         best_k = n
+#         best_accuracy = accuracy
+# print(best_k, best_accuracy)
+
+accuracy = knn(5, x_train, x_test, tag_train, tag_test)
 
 endtime = datetime.datetime.now()
 print((endtime - starttime).seconds)
